5628eefa38a627175b0e4a27b80e6ad570cecd99
main-c <yannik.kadjie@facsciences-uy1.cm>
2021-09-10 11:44:38 +0100

    creating django project

---
+++ src/electroniccore/core/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, models.CASCADE)
+    phone = models.BigIntegerField()
+
+
+class Product(models.Model):
+
+    STATUS = (
+        ('New', 'New'),
+        ('On Sale', 'On Salee'),
+        ('Reached Limits', 'Reached Limits')
+    )
+
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    slug = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=100, choices=STATUS, default='New')
+
+
+class ProductImage(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(upload_to='products_image')
+
+
+class Order(models.Model):
+    bill_to = models.CharField(max_length=200)
+    phone = models.BigIntegerField()
+    note = models.TextField()
+    ordered_on = models.DateTimeField(auto_now=True)
+    delivered = models.BooleanField()
+
+
+class Payment(models.Model):
+    pass
+
+
+class Cart(models.Model):
+    pass
+
+
+class FeedBack(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class ShippingAdress(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
