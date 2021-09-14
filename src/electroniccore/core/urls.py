b12a8a61b8b91145ca93aeca5f477348211708ab
Jugalux <jugalux111@gmail.com>
2021-09-14 17:40:08 +0100

    gestion erreurs avec fusion backend

--- src/electroniccore/core/urls.py
+++ src/electroniccore/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from core.views import (HomeView, AccountView, ShopView, ProductView,
-                        CheckoutView, PaymentView, Login,
-                        Logout, SignupView,
+                        CartView, CheckoutView, PaymentView, LoginView,
+                        LogoutView, SignupView,
                         )
 
 
@@ -10,13 +10,12 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('my-account/', AccountView.as_view(), name='account'),
     path('shop/', ShopView.as_view(), name='shop'),
-    path('product/<str:product_slug>', ProductView.as_view(), name='product'),
-
+    path('product/', ProductView.as_view(), name='product'),
+    path('cart/', CartView.as_view(), name='cart'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('payment/', PaymentView.as_view(), name='payment'),
-
-    path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('signin/', SignupView.as_view(), name='signin'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', SignupView.as_view(), name='signup'),
 
 ]
--- src/electroniccore/core/views.py
+++ src/electroniccore/core/views.py
@@ -18,12 +18,41 @@ class HomeView(TemplateView):
     template_name = "core/home.html"
 
 
-class AccountView(TemplateView):  # Dashboard
+class AccountView(TemplateView): #Dashboard
     template_name = "core/account.html"
 
 
 class ShopView(TemplateView):
     template_name = "core/shop.html"
+
+
+class ProductView(TemplateView):
+    template_name = "core/product.html"
+
+
+class CartView(TemplateView): #Panier
+    template_name = "core/cart.html"
+
+class CheckoutView(TemplateView): #Formulaire validation commande
+    template_name = "core/checkout.html"
+
+
+class PaymentView(TemplateView):
+    template_name = "core/payment.html"
+
+
+class LoginView(TemplateView):
+    template_name = "core/login.html"
+
+
+class LogoutView(TemplateView):
+    template_name = "core/logout.html"
+
+
+class SignupView(TemplateView):
+    template_name = "core/signup.html"
+
+
 
 
 class CateorygView(View):
